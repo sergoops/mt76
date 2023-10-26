826d39e23a11a5ce2f8df20e5696bf980825b812
MeiChia Chiu <meichia.chiu@mediatek.com>
2023-10-26 21:11:05 +0800

    wifi: mt76: mt7915: Fixed null pointer dereference issue
    Without this patch, when the station is still in Authentication stage and
    sends a "Notify bandwidth change action frame" to AP at the same time,
    there will be a race condition that causes a crash to occur because the AP
    access "msta->vif" that has not been fully initialized.
    
    Signed-off-by: Bo Jiao <Bo.Jiao@mediatek.com>
    Signed-off-by: Money Wang <money.wang@mediatek.com>
    Signed-off-by: MeiChia Chiu <meichia.chiu@mediatek.com>

--- mt7915/main.c
+++ mt7915/main.c
@@ -1234,6 +1234,12 @@ static void mt7915_sta_rc_update(struct ieee80211_hw *hw,
 	if (!msta->wcid.sta)
 		return;
 
+	if (!msta->vif) {
+		dev_warn(dev->mt76.dev, "Un-initialized STA %pM wcid %d in rc_work\n",
+			 sta->addr, msta->wcid.idx);
+		return;
+	}
+
 	mt7915_sta_rc_work(&changed, sta);
 	ieee80211_queue_work(hw, &dev->rc_work);
 }
